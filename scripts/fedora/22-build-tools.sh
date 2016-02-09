7b64935ad82d29f7b9f4e2368a181822a58bc676
Sven Nierlein <sven@nierlein.de>
2016-02-09 13:43:51 +0100

    fix i386 fedora23 installation

--- scripts/fedora/22-build-tools.sh
+++ scripts/fedora/22-build-tools.sh
@@ -1,3 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash
 # Installing build tools here because Fedora 22 will not do so during kickstart
 dnf -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
+yum -y install gcc dkms "kernel-devel-uname-r == $(uname -r)"
