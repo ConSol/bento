95fe3bbe5bd434d52df106b1497421cc57d49c76
Sven Nierlein <sven@nierlein.de>
2016-09-16 10:05:25 +0200

    add sles11sp4

--- scripts/common/virtualbox.sh
+++ scripts/common/virtualbox.sh
@@ -7,12 +7,12 @@ virtualbox-iso|virtualbox-ovf)
     echo "Virtualbox Tools Version: $VER";
 
     mkdir -p /tmp/vbox;
-    mount -o loop $HOME_DIR/VBoxGuestAdditions_${VER}.iso /tmp/vbox;
+    mount -o loop ~vagrant/VBoxGuestAdditions_${VER}.iso /tmp/vbox;
     sh /tmp/vbox/VBoxLinuxAdditions.run \
         || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
             "For more read https://www.virtualbox.org/ticket/12479";
     umount /tmp/vbox;
     rm -rf /tmp/vbox;
-    rm -f $HOME_DIR/*.iso;
+    rm -f ~vagrant/*.iso;
     ;;
 esac
